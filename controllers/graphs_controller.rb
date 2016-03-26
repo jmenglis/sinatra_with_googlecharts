09805192029a1ae7091c6e37f25007519b139580
Josh English <josh@joshenglish.com>
2016-03-26 18:21:13 -0500

    updating site with google charts

--- controllers/graphs_controller.rb
+++ controllers/graphs_controller.rb
@@ -1,2 +1,13 @@
 class GraphsController < ApplicationController
+  get '/' do
+    image = Gchart.line(
+      :size => '600x500',
+      :title => 'Challenges in Web Development',
+      :bg => 'efefef',
+      :legend => ['Difficulty of Code'],
+      :data => [10, 30, 120, 45, 72]
+    )
+    erb :test, locals: { image: image }
+  end
+
 end
